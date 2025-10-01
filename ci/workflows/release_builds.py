511ff9935a5fe08e1347a90427302f1359d18508
strtgbb <146047128+strtgbb@users.noreply.github.com>
2025-10-01 12:46:11 -0400

    partially re-enable version_log hook

--- ci/workflows/release_builds.py
+++ ci/workflows/release_builds.py
@@ -58,7 +58,7 @@ workflow = Workflow.Config(
     enable_commit_status_on_failure=True,
     pre_hooks=[
         # "python3 ./ci/jobs/scripts/workflow_hooks/store_data.py",
-        # "python3 ./ci/jobs/scripts/workflow_hooks/version_log.py",
+        "python3 ./ci/jobs/scripts/workflow_hooks/version_log.py",
     ],
     workflow_filter_hooks=[should_skip_job],
     post_hooks=[],
