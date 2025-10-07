aa124681c167167ba5c4d9e8bfede91c326243db
strtgbb <146047128+strtgbb@users.noreply.github.com>
2025-10-07 11:34:32 -0400

    Rebase CICD on v25.8.9.20-lts

---
+++ ci/workflows/release_builds.py
@@ -0,0 +1,52 @@
+from praktika import Workflow
+
+from ci.defs.defs import DOCKERS, SECRETS, ArtifactConfigs
+from ci.defs.job_configs import JobConfigs
+from ci.jobs.scripts.workflow_hooks.filter_job import should_skip_job
+
+builds_for_release_branch = [
+    job.unset_provides("unittest")
+    for job in JobConfigs.build_jobs
+    if "coverage" not in job.name
+]
+
+workflow = Workflow.Config(
+    name="Release Builds",
+    event=Workflow.Event.DISPATCH,
+    jobs=[
+        *builds_for_release_branch,
+        JobConfigs.docker_sever,
+        JobConfigs.docker_keeper,
+        *JobConfigs.install_check_master_jobs,
+        *[
+            job
+            for job in JobConfigs.functional_tests_jobs
+            if any(t in job.name for t in ("release", "binary"))
+        ],
+    ],
+    additional_jobs=["GrypeScan", "SignRelease", "CIReport"],
+    artifacts=[
+        *ArtifactConfigs.clickhouse_binaries,
+        *ArtifactConfigs.clickhouse_stripped_binaries,
+        *ArtifactConfigs.clickhouse_debians,
+        *ArtifactConfigs.clickhouse_rpms,
+        *ArtifactConfigs.clickhouse_tgzs,
+    ],
+    dockers=DOCKERS,
+    secrets=SECRETS,
+    enable_job_filtering_by_changes=True,
+    enable_cache=True,
+    enable_report=True,
+    enable_cidb=True,
+    enable_commit_status_on_failure=True,
+    pre_hooks=[
+        "python3 ./ci/jobs/scripts/workflow_hooks/store_data.py",
+        "python3 ./ci/jobs/scripts/workflow_hooks/version_log.py",
+    ],
+    workflow_filter_hooks=[should_skip_job],
+    post_hooks=[],
+)
+
+WORKFLOWS = [
+    workflow,
+]
