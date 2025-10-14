0828fe9df2b28c66468323adb85eeadeebb4e9ba
Arthur Passos <arthur.ti@outlook.com>
2025-10-14 13:21:49 -0300

    settings change histr

--- src/Core/SettingsChangesHistory.cpp
+++ src/Core/SettingsChangesHistory.cpp
@@ -47,6 +47,8 @@ const VersionToSettingsChangesMap & getSettingsChangesHistory()
             {"object_storage_cluster", "", "", "New setting"},
             {"object_storage_max_nodes", 0, 0, "New setting"},
             {"allow_retries_in_cluster_requests", false, false, "New setting"},
+	    {"allow_experimental_export_merge_tree_part", false, false, "New setting."},
+	    {"export_merge_tree_part_overwrite_file_if_exists", false, false, "New setting."},
         });
         addSettingsChanges(settings_changes_history, "25.8",
         {
